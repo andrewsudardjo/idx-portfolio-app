1c3a406bc392ef5d360c843cde1eafbc4784128e
andrewsudardjo <sudardjoandrew123@gmail.com>
2026-01-04 16:23:54 +0800

    Ignore env file

--- OneDrive/Desktop/idx project - node.js/app.js
+++ OneDrive/Desktop/idx project - node.js/app.js
@@ -1,18 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const stockRoutes = require("./routes/stockRoutes");
 
-const dbURI =
-  "mongodb+srv://netninja:test123@nodejs.iqkf8al.mongodb.net/StockDB";
 mongoose
-  .connect(dbURI)
-  .then((result) => {
-    app.listen(3001);
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 3001);
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
